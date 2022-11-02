53c525ce6c3a768a4a9c1b15bea90e7e7dcfb689
Jaewook <eari100@naver.com>
2022-11-02 18:54:06 +0900

    feat: 선언적 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloWorld from './components/docs/Chap1-introduction/HelloWorld.vue';
+import DeclarativeRenderingVue from './components/docs/Chap1-introduction/DeclarativeRendering.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld message="Hello Vue!!" />
+      <DeclarativeRenderingVue />
     </div>
   </header>
 
---
+++ src/components/docs/Chap1-introduction/DeclarativeRendering.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const message = `You loaded this page on ${new Date().toLocaleString()}`
+</script>
+
+<template>
+    <div id="bind-attribute">
+        <span v-bind:title="message">
+            여기에 마우스를 올려두고 잠시 기다리면 제목이 동적으로 바뀝니다!
+        </span>
+    </div>
+</template>
+
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
