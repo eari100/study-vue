019194b86c7ab60cf6cdfba3173a627da0f7978e
Jaewook <eari100@naver.com>
2022-11-02 17:55:53 +0900

    refactor: Hello World 컴포넌트 리펙토링

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/docs/Chap1-introduction/HelloWorld.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="Hello World!" />
+      <HelloWorld message="Hello Vue!!" />
     </div>
   </header>
 
@@ -18,30 +15,4 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/docs/Chap1-introduction/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div class="demo">
+        {{ message }}
+    </div>
+</template>
+
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
