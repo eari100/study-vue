dda7e38814959d24bdc698620c0765934fb01ed6
Jaewook <eari100@naver.com>
2022-11-03 13:35:40 +0900

    feat: 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
-import DeclarativeRenderingVue from './components/docs/Chap1-introduction/DeclarativeRendering.vue'
-import UserInputHandling2 from './components/docs/Chap1-introduction/UserInputHandling-2.vue';
-import ConditionalStatementsAndLoops1 from './components/docs/Chap1-introduction/ConditionalStatementsAndLoops-1.vue'; './components/docs/Chap1-introduction/ConditionalStatementsAndLoops-1.vue'
+import ConditionalStatementsAndLoops2 from './components/docs/Chap1-introduction/ConditionalStatementsAndLoops-2.vue';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <ConditionalStatementsAndLoops1 />
+      <ConditionalStatementsAndLoops2 />
     </div>
   </header>
 
---
+++ src/components/docs/Chap1-introduction/ConditionalStatementsAndLoops-2.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data() {
+            return {
+                todos: [
+                    { text: 'Learn JavaScript' },
+                    { text: 'Learn Vue' },
+                    { text: 'Build something awesome' }
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <div id="list-rendering">
+        <ol>
+            <li v-for="todo in todos">
+            {{ todo.text }}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
