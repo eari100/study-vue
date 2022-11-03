c18295266723e7aebd3edfbec2e4b21e1b3d3310
Jaewook <eari100@naver.com>
2022-11-03 09:54:47 +0900

    feat: 사용자 입력 핸들링
    - reverse 예제

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import DeclarativeRenderingVue from './components/docs/Chap1-introduction/DeclarativeRendering.vue'
+import UserInputHandling1 from './components/docs/Chap1-introduction/UserInputHandling-1.vue';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <DeclarativeRenderingVue />
+      <UserInputHandling1 />
     </div>
   </header>
 
---
+++ src/components/docs/Chap1-introduction/UserInputHandling-1.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello Vue.js!'
+            }
+        },
+        methods: {
+            reverseMessage() {
+            this.message = this.message
+                .split('')
+                .reverse()
+                .join('')
+            }
+        }
+    }
+</script>
+
+<template>
+    <div id="event-handling" class="demo">
+        <p>{{ message }}</p>
+        <button @click="reverseMessage">Reverse Message</button>
+    </div>
+</template>
+
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
